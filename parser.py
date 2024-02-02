5f87b545154d2d1d8524654d4d639a7af9d7e449
Dmitry V. Sokolov <ha@haqr.eu>
2024-02-02 17:17:05 +0100

    grammar cleaning

--- parser.py
+++ parser.py
@@ -17,76 +17,75 @@ class WendParser(Parser):
          ('right', UPLUS)
     )
 
-    @_('fun_decl fun_body')
+    @_('FUN ID LPAREN param_list RPAREN type_optional BEGIN var_list fun_list statement_list END')
     def fun(self, p):
-        return Function(*(p.fun_decl[:-1] + p.fun_body), p.fun_decl[-1])
+        return Function(p[1], p.param_list or [], p.var_list or [],  p.fun_list or [], p.statement_list or [], {'type':p.type_optional})
 
-    @_('FUN ID LPAREN [ param_list ] RPAREN [ COLON type ]')
-    def fun_decl(self, p):
-        deco, name, params = {'type':(p.type or Type.VOID), 'lineno':p.lineno}, p[1], (p.param_list or [])
-        return [name, params, deco]
-
-    @_('param { COMMA param }')
+    @_('param_list COMMA param')
     def param_list(self, p):
-        return [p.param0] + p.param1
-
-    @_('ID COLON type')
-    def param(self, p):
-        return (p[0], {'type':p.type, 'lineno':p.lineno})
-
-    @_('BEGIN [ var_list ] [ fun_list ] statement_list_optional END')
-    def fun_body(self, p):
-        return [p.var_list or [], p.fun_list or [], p.statement_list_optional or []]
-
-    @_('var { var }')
-    def var_list(self, p):
-        return [p.var0] + p.var1
-
-    @_('VAR ID COLON type SEMICOLON')
-    def var(self, p):
-        return (p[1], {'type':p.type, 'lineno':p.lineno})
-
-    @_('fun { fun }')
-    def fun_list(self, p):
-        return [p.fun0] + p.fun1
-
-    @_('statement { statement }')
-    def statement_list(self, p):
-        return [p.statement0] + p.statement1
-
-    @_('WHILE expr BEGIN statement_list_optional END')
-    def statement(self, p):
-        return While(p.expr, p.statement_list_optional or [], {'lineno':p.lineno})
-
-    @_('IF expr BEGIN statement_list_optional END [ ELSE BEGIN statement_list_optional END ]')
-    def statement(self, p):
-        return IfThenElse(p.expr, p.statement_list_optional0 or [], p.statement_list_optional1 or [], {'lineno':p.lineno})
-
-    @_('statement_list')                  # sly does not support nested
-    def statement_list_optional(self, p): # optional targets, therefore
-        return p.statement_list           # this rule
+        return p.param_list + [ p.param ]
 
     @_('')
-    def statement_list_optional(self, p):
+    def param_list(self, p):
+        return [ ]
+
+    @_('param')
+    def param_list(self, p):
+        return [ p.param ]
+
+    @_('ID COLON TYPE')
+    def param(self, p):
+        return (p[0], {'type':Type.INT if p[2]=='int' else Type.BOOL, 'lineno':p.lineno})
+
+    @_('var_list var')
+    def var_list(self, p):
+        return p.var_list + [p.var]
+
+    @_('')
+    def var_list(self, p):
         return []
+
+    @_('VAR ID COLON TYPE SEMICOLON')
+    def var(self, p):
+        return (p[1], {'type':Type.INT if p[3]=='int' else Type.BOOL, 'lineno':p.lineno})
+
+    @_('fun_list fun')
+    def fun_list(self, p):
+        return p.fun_list + [p.fun]
+
+    @_('')
+    def fun_list(self, p):
+        return []
+
+    @_('statement_list statement')
+    def statement_list(self, p):
+        return p.statement_list + [p.statement]
+
+    @_('')
+    def statement_list(self, p):
+        return []
+
+    @_('WHILE expr BEGIN statement_list END')
+    def statement(self, p):
+        return While(p.expr, p.statement_list or [], {'lineno':p.lineno})
+
+    @_('IF expr BEGIN statement_list END [ ELSE BEGIN statement_list END ]')
+    def statement(self, p):
+        return IfThenElse(p.expr, p.statement_list0 or [], p.statement_list1 or [], {'lineno':p.lineno})
 
     @_('PRINT expr SEMICOLON')
     def statement(self, p):
         return Print(p.expr, p[0]=='println', {'lineno':p.lineno})
 
-    @_('RETURN SEMICOLON')
+    @_('RETURN [ expr ] SEMICOLON')
     def statement(self, p):
-        return Return(None, {'lineno':p.lineno})
-
-    @_('RETURN expr SEMICOLON')
-    def statement(self, p):
-        return Return(p.expr, {'lineno':p.lineno})
+        return Return(p.expr or None, {'lineno':p.lineno})
 
     @_('ID ASSIGN expr SEMICOLON')
     def statement(self, p):
         return Assign(p[0], p.expr, {'lineno':p.lineno})
 
-    @_('ID LPAREN [ args ] RPAREN SEMICOLON')
+    @_('ID LPAREN args RPAREN SEMICOLON')
     def statement(self, p):
         return FunCall(p[0], p.args or [], {'lineno':p.lineno})
 
@@ -121,13 +120,21 @@ class WendParser(Parser):
     def expr(self, p):
         return Var(p[0], {'lineno':p.lineno})
 
-    @_('ID LPAREN [ args ] RPAREN')
+    @_('ID LPAREN args RPAREN')
     def expr(self, p):
         return FunCall(p[0], p.args or [], {'lineno':p.lineno})
 
-    @_('expr { COMMA expr }')
+    @_('args COMMA expr')
     def args(self, p):
-        return [p.expr0] + p.expr1
+        return p.args + [ p.expr ]
+
+    @_('')
+    def args(self, p):
+        return []
+
+    @_('expr')
+    def args(self, p):
+        return [ p.expr ]
 
     @_('STRING')
     def expr(self, p):
@@ -141,9 +148,13 @@ class WendParser(Parser):
     def expr(self, p):
         return Boolean(p.BOOLVAL=='true', {'lineno':p.lineno})
 
-    @_('TYPE')
-    def type(self, p):
-        return Type.INT if p[0]=='int' else Type.BOOL
+    @_('COLON TYPE')
+    def type_optional(self, p):
+        return Type.INT if p[1]=='int' else Type.BOOL
+
+    @_('')
+    def type_optional(self, p):
+        return Type.VOID
 
     def error(self, token):
         if not token:
