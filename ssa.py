f7f06acb7da10db2b313f4b9bc4d373a0e5d06f6
Dmitry V. Sokolov <ha@haqr.eu>
2025-02-07 10:35:49 +0100

    mem2reg wip, TODO: successors/predecessors

--- ssa.py
+++ ssa.py
@@ -16,31 +16,30 @@ class Phi:
 class BasicBlock:
     def __init__(self, label):
         self.label = label
-        self.instr  = []
+        self.instructions  = []
         self.phi_functions = {} # map variable -> { Phi object }
-        self.successors    = [] # do we need successors? branch/jump instr may suffice
-        self.predecessors  = [] # no need for those, can compute on the fly
+        self.successors    = set() # do we need successors? branch/jump instr may suffice
+        self.predecessors  = set() # no need for those, can compute on the fly
         self.replacements  = {} # TODO: incorporate replacements into individual instr
 
     def add_successor(self, block):
         if block not in self.successors:
-            self.successors.append(block)
-            block.predecessors.append(self)
+            self.successors.add(block)
+            block.predecessors.add(self)
 
     def add_phi_function(self, reg):
         self.phi_functions[reg] = Phi(reg + '_' + self.label)
 
+    def replace(self):
+        for i in self.instructions:
+            i.a, i.b, i.c = ( self.replacements.get(s, s) for s in (i.a, i.b, i.c) )
+        self.replacements = {}
+
     def __repr__(self):
-        def find_and_replace(string, replacements):
-            for find, replace in replacements.items():
-                string = string.replace(find, str(replace)) # TODO can we have numerical and boolean constants in replace?
-            return string
-        result = f'{self.label}:\n'
-        for phi in self.phi_functions.values():
-            result += f'{phi}\n'
-        for i in self.instr:
-            result += find_and_replace(f'{i}\n', self.replacements)
-        return result
+        self.replace()
+        return f'{self.label}:\n' + \
+                ''.join( [ f'{phi}\n' for phi in self.phi_functions.values() ] ) + \
+                ''.join( [ f'{i}\n' for i in self.instructions ] )
 
 class ControlFlowGraph:
     def __init__(self):
@@ -52,7 +51,7 @@ class ControlFlowGraph:
 
     def compute_adjacency(self):
         for b in self.blocks.values():
-            i = b.instr[-1]
+            i = b.instructions[-1]
             for succ in [i.a] if 'br label' in i.str else [i.b, i.c]:
                 if succ:
                     self.add_edge(b.label, succ)
@@ -75,10 +74,10 @@ class ControlFlowGraph:
                 if b not in visited: dfs(b, visited, postorder)
             postorder.append(block)
         postorder = []                                              # for efficiency reasons, we visit blocks in a way that ensures
-        dfs(self.blocks['entry'], set(), postorder)                # processing of each block after its predecessors have been processed
+        dfs(self.blocks['entry'], set(), postorder)                 # processing of each block after its predecessors have been processed
 
         dom = { b : set(self.blocks.values()) for b in self.blocks.values() }
-        dom[ self.blocks['entry'] ] = { self.blocks['entry'] }    # dom[b] contains every block that dominates b
+        dom[ self.blocks['entry'] ] = { self.blocks['entry'] }      # dom[b] contains every block that dominates b
         changed = True
         while changed:                                              # the iterative dominator algorithm [Cooper, Harvey and Kennedy, 2006]
             changed = False
@@ -88,7 +87,7 @@ class ControlFlowGraph:
                     dom[b] = new_dom
                     changed = True
 
-        idom = { self.blocks['entry'] : None }                     # idom[b] contains exactly one block, the immediate dominator of b
+        idom = { self.blocks['entry'] : None }                      # idom[b] contains exactly one block, the immediate dominator of b
         for b in postorder[-2::-1]:                                 # reverse or not we do not care here, but do not visit the source block
             idom[b] = max(dom[b] - {b}, key=lambda x: len(dom[x]))  # immediate dominator is the one with the maximum number of dominators (except the block itself)
 
@@ -104,10 +103,10 @@ class ControlFlowGraph:
     def mem2reg(self):
         self.compute_dominance_frontier()
 
-        variables = [ i.ptr for i in self.blocks['entry'].instr if 'alloca' in i.str ]
+        variables = [ i.a for i in self.blocks['entry'].instructions if 'alloca' in i.str ]
         phi = { v:set() for v in variables }                        # map (variable -> set of basic blocks)
         for v in variables:
-            blocks_with_store = { b for b in self.blocks.values() for i in b.instr if isinstance(i, Store) and i.ptr==v }
+            blocks_with_store = { b for b in self.blocks.values() for i in b.instructions if 'store' in i.str and i.b==v }
             blocks_to_consider = blocks_with_store.copy()
             while blocks_to_consider:
                 block = blocks_to_consider.pop()
@@ -116,7 +115,6 @@ class ControlFlowGraph:
                     if frontier not in blocks_with_store:
                         blocks_to_consider.add(frontier)
 
-        print(phi)
         for v, bb in phi.items():                                   # insert phi nodes (for the moment without choices)
             for b in bb:
                 b.add_phi_function(v)
@@ -133,25 +131,24 @@ class ControlFlowGraph:
             if block in visited: return                             # we need to revisit blocks with phi functions as many times as we have incoming edges,
             visited.add(block)                                      # therefore the visited check is made after the choice placement
 
-            for i in block.instr[:]:                         # iterate through a copy since we modify the list
-                match i:
-                    case Load():
-                        _, val = find_variable(i.ptr, stack)
-                        block.instr.remove(i)
-                        block.replacements[i.reg] = val
-                    case Store():
-                        stack[-1][i.ptr] = (block.label, i.value)
-                        block.instr.remove(i)
-                    case Branch():
+            for i in block.instructions[:]:                         # iterate through a copy since we modify the list
+                match i.str:
+                    case _ if 'load' in i.str:
+                        _, val = find_variable(i.b, stack)
+                        block.instructions.remove(i)
+                        block.replacements[i.a] = val
+                    case _ if 'store' in i.str:
+                        stack[-1][i.b] = (block.label, i.a)
+                        block.instructions.remove(i)
+                    case _ if 'br i1' in i.str:
                         stack.append({})
-                        store_load(self.blocks[i.ilabel], visited, stack)
+                        store_load(self.blocks[i.b], visited, stack)
                         stack.pop()
                         stack.append({})
-                        store_load(self.blocks[i.elabel], visited, stack)
+                        store_load(self.blocks[i.c], visited, stack)
                         stack.pop()
-                    case Jump():
-                        store_load(self.blocks[i.label],  visited, stack)
-
+                    case _ if 'br label' in i.str:
+                        store_load(self.blocks[i.a],  visited, stack)
         stack = [{}] # stack of maps (variable -> (block, value)); necessary for storing context while branching
         store_load(self.blocks['entry'], set(), stack)
 
@@ -166,52 +163,52 @@ cfg.add_block('bb6')
 cfg.add_block('bb7')
 cfg.add_block('bb8')
 
-cfg.blocks['entry'].instr = [
+cfg.blocks['entry'].instructions = [
     Instruction('\t{a} = alloca {t}', 'i32', '%a'),
     Instruction('\tbr label %{a}', None, 'bb1'),
 ]
 
-cfg.blocks['bb1'].instr = [
+cfg.blocks['bb1'].instructions = [
     Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '1', '%a'),
     Instruction('\tbr i1 %{a}, label %{b}, label %{c}', None, 'foo', 'bb2', 'bb4'),
 ]
 
-cfg.blocks['bb2'].instr = [
+cfg.blocks['bb2'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a0', '%a'),
     Instruction('\t{a} = add {t} {b}, {c}', 'i32', '%t0', '%a0', '1'),
-    Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '1', '%a'),
+    Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '%t0', '%a'),
     Instruction('\tbr label %{a}', None, 'bb3'),
 ]
 
-cfg.blocks['bb3'].instr = [
+cfg.blocks['bb3'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a1', '%a'),
     Instruction('\t{a} = add {t} {b}, {c}', 'i32', '%t1', '%a1', '%a1'),
     Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '%t1', '%a'),
     Instruction('\tbr i1 %{a}, label %{b}, label %{c}', None, 'foo', 'bb8', 'bb2'),
 ]
 
-cfg.blocks['bb4'].instr = [
+cfg.blocks['bb4'].instructions = [
     Instruction('\tbr i1 %{a}, label %{b}, label %{c}', None, 'foo', 'bb5', 'bb6'),
 ]
 
-cfg.blocks['bb5'].instr = [
+cfg.blocks['bb5'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a2', '%a'),
     Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '2', '%a'),
     Instruction('\tbr label %{a}', None, 'bb7'),
 ]
 
-cfg.blocks['bb6'].instr = [
+cfg.blocks['bb6'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a3', '%a'),
     Instruction('\tstore {t} {a}, {t}* {b}', 'i32', '3', '%a'),
     Instruction('\tbr label %{a}', None, 'bb7'),
 ]
 
-cfg.blocks['bb7'].instr = [
+cfg.blocks['bb7'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a4', '%a'),
     Instruction('\tbr label %{a}', None, 'bb8'),
 ]
 
-cfg.blocks['bb8'].instr = [
+cfg.blocks['bb8'].instructions = [
     Instruction('\t{a} = load {t}, {t}* {b}', 'i32', '%a5', '%a'),
     Instruction('\tret {a}', None, '%a5'),
 ]
@@ -219,7 +216,7 @@ cfg.blocks['bb8'].instr = [
 print(cfg)
 cfg.compute_adjacency()
 
-#cfg.mem2reg()
+cfg.mem2reg()
 
-#print(cfg)
+print(cfg)
 
