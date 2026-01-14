217c205fe4a5156e3a7c189817bc586b50f09ae9
Dmitry Sokolov <dmitry.sokolov@univ-lorraine.fr>
2026-01-14 13:39:35 +0100

    lexer polishing

--- lexer.py
+++ lexer.py
@@ -14,53 +14,42 @@ class WendLexer:
     def tokenize(self, text):
         lineno, idx, state, accum = 0, 0, 0, ''
         while idx<len(text):
-            sym1 = text[idx+0] if idx<len(text)-0 else ' '
-            sym2 = text[idx+1] if idx<len(text)-1 else ' '
-            if state==0: # start scanning a new token
-                if sym1 == '/' and sym2 == '/':   # start a comment scan
+            sym1,sym2 = (text+' ')[idx:idx+2]                                   # current character and the next one
+            idx += 1
+            if state==0:                                                        # start scanning a new token
+                if sym1 == '/' and sym2 == '/':                                 # start a comment scan
                     state = 1
-                elif sym1.isalpha() or sym1=='_': # start a word scan
+                elif sym1.isalpha() or sym1=='_':                               # start a word scan
                     state = 3
-                    accum += sym1
-                elif sym1.isdigit():              # start a number scan
+                elif sym1.isdigit():                                            # start a number scan
                     state = 4
-                    accum += sym1
-                elif sym1 == '"':                 # start a string scan
+                elif sym1 == '"':                                               # start a string scan
                     state = 2
-                elif sym1 + sym2 in self.double_char:  # emit two-character token
+                elif sym1 + sym2 in self.double_char:                           # emit a two-character token
                     yield Token(self.double_char[sym1+sym2], sym1+sym2, lineno)
                     idx += 1
-                elif sym1 in self.single_char:         # emit one-character token
+                elif sym1 in self.single_char:                                  # emit a one-character token
                     yield Token(self.single_char[sym1], sym1, lineno)
-                elif sym1 not in ['\r', '\t', ' ', '\n']: # ignore whitespace
+                elif sym1 not in ['\r', '\t', ' ', '\n']:                       # ignore whitespace
                     raise Exception(f'Lexical error: illegal character \'{sym1}\' at line {lineno}')
-            elif state==3:                                          # scanning a word, check next character
-                if sym1.isalpha() or sym1=='_' or  sym1.isdigit():  # still word?
-                    accum += sym1                                   # if yes, continue
-                else:                                               # otherwise the scan stops, we have a word
-                    if accum in self.keywords:                           # is the word reserved?
-                        yield Token(self.keywords[accum], accum, lineno) # if yes, keyword
-                    else:
-                        yield Token('ID', accum, lineno)            # identifier otherwise
-                    idx -= 1
-                    state, accum = 0, '' # start new scan
-            elif state==4:                                          # scanning a number
-                if sym1.isdigit():                                  # is next character a digit?
-                    accum += sym1                                   # if yes, continue
-                else:
-                    yield Token('INTEGER', accum, lineno)           # otherwise, emit number token
-                    idx -= 1
-                    state, accum = 0, '' # start new scan
-            elif state==2:                                          # scanning a string, check next character
-                if sym1 != '"' or accum and accum[-1]=='\\':        # if not quote mark (or if escaped quote mark),
-                    accum += sym1                                   # continue the scan
-                else:
-                    yield Token('STRING', accum, lineno) # otherwise emit the token
-                    state, accum = 0, '' # start new scan
+            if state==2:                                                        # scanning a string
+                accum += sym1
+                if sym2 == '"' and (not accum or accum[-1]!='\\'):              # if not-escaped quote mark,
+                    yield Token('STRING', accum[1:], lineno)                    # emit the token, do not forget to drop the opening quote
+                    state, accum, idx = 0, '', idx+1                            # start a new scan, skip the ending quote
+            elif state==3:                                                      # scanning a word
+                accum += sym1
+                if not (sym2.isalpha() or sym2=='_' or  sym2.isdigit()):        # still a word?
+                    yield Token(self.keywords.get(accum, 'ID'), accum, lineno)  # if not, keyword or ID
+                    state, accum = 0, ''                                        # start a new scan
+            elif state==4:                                                      # scanning a number
+                accum += sym1
+                if not sym2.isdigit():                                          # is next character a digit?
+                    yield Token('INTEGER', accum, lineno)                       # if not, emit number token
+                    state, accum = 0, ''                                        # start a new scan
             if sym1 == '\n':
                 lineno += 1
-                if state==1: # if comment, start new scan
+                if state==1:                                                    # if comment, start new scan
                     state, accum = 0, ''
-            idx += 1
         if state:
             raise Exception('Lexical error: unexpected EOF')
